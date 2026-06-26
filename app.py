08452c43c7718daec8d82173214e36e5dbb9a492
ArshiyaAnjum18 <arshiyaanjum0918@gmail.com>
2026-06-26 21:41:16 +0530

    Fix Render Flask deployment

--- app.py
+++ app.py
@@ -45,6 +45,13 @@ def create_app():
     return app
 
 
+# Render / Gunicorn entry point
+application = create_app()
+
+
 if __name__ == '__main__':
-    application = create_app()
-    application.run(debug=False, host='0.0.0.0', port=5000)
+    application.run(
+        debug=False,
+        host='0.0.0.0',
+        port=5000
+    )
